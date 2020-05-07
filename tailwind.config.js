b3719ffe38bd03aac9e7b8d3d12d35223d17a7a3
Tomasz Bujnowicz <tomasz@prollective.com>
2020-05-07 17:40:12 +0100

    Add docs link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// Docs: https://tailwindcss.com/docs/configuration
+// Default config: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+
 module.exports = {
   theme: {
     extend: {}
